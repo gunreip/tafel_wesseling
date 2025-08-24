177431d7f30051d2e996657a68e903a251a5f308
gunreip <gunreip@web.de>
2025-08-24 15:19:32 +0200

    chore: initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+
+// Lucide Icons
+import { createIcons, icons } from 'lucide';
+document.addEventListener('DOMContentLoaded', () => { try { createIcons({ icons }); } catch (e) {} });
